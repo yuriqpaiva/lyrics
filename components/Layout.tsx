62cc4797c103cd0d8f19fcc7db37fca3db64f104
yuriqpaiva <yuriqpaiva@gmail.com>
2021-08-23 14:28:29 -0300

    Update estilo

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,4 @@
+import { songIcon } from "../public/icons/icons";
 import useAppData from "../src/hook/useAppData";
 import Button from "./Button";
 import Input from "./Input";
@@ -23,12 +24,18 @@ export default function Layout(props: LayoutProps) {
            bg-white rounded-md shadow-lg w-11/12 my-3 
 
         `}>
-            <div className='bg-blue-100 w-full rounded-t pt-5 border-b-2 border-gray-300'>
+            <div className={`
+            flex justify-center bg-blue-100 w-full 
+            rounded-t pt-5 border-b-2 border-gray-300
+            
+            `}>
+
                 <h1 className={`
                 text-center mb-5 text-4xl font-semibold
                 `}>{props.title}</h1>
+                <span className={`mx-3`}>{songIcon}</span>
             </div>
-            
+
             <div className={`flex flex-col mt-5 lg:w-4/12 md:w-2/4 w-72`}>
                 <Input
                     title='Artista: '
@@ -46,10 +53,20 @@ export default function Layout(props: LayoutProps) {
                 />
                 <Button
                     value='Procurar'
-                    onClick={catchLyrics} 
-                    />
-                <Lyrics dadosApi={dadosApi} erro={erro}/>
-                <p className='self-center my-3 text-center'>@2021 - Desenvolvido por Yuri Paiva </p>
+                    onClick={catchLyrics}
+                />
+                <Lyrics dadosApi={dadosApi} erro={erro} />
+                <p className='self-center my-3 text-center'>@2021 - Desenvolvido por
+                    <a
+                        href="https://github.com/yuriqpaiva"
+                        className={`
+                        text-blue-800 font-semibold 
+                        hover:text-blue-500
+                        `}
+                        rel='noreferrer'
+                        target='_blank'
+                    > Yuri Paiva</a>
+                </p>
             </div>
         </div>
     )
